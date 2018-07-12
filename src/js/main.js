972fda39381a875e0e1afbca9814f854e900b2db
Kyle Lawson <Kyle.Lawson7@yahoo.com>
2018-07-11 22:54:45 -0700

    added mobile scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,10 @@ var scrolling = false
 var itemElements = document.getElementsByClassName('item')
 var projectElements = document.getElementsByClassName('project-container')
 
+window.onLoad = function() {
+	window.scrollTo(0, 1)
+}
+
 window.onItemClick = function(x) {
 	if (x == 0) {
 		document.getElementById('down-arrow-svg').classList.remove('blastoff')
@@ -108,4 +112,61 @@ window.onDownArrowClick = function() {
 	setTimeout(function() {
 		onItemClick(1)
 	}, 800)
+}
+
+document.addEventListener('touchstart', handleTouchStart, false)
+document.addEventListener('touchmove', handleTouchMove, false)
+
+var xDown = null
+var yDown = null
+
+function handleTouchStart(evt) {
+	xDown = evt.touches[0].clientX
+	yDown = evt.touches[0].clientY
+}
+
+function handleTouchMove(evt) {
+	if (!xDown || !yDown) {
+		return
+	}
+
+	var xUp = evt.touches[0].clientX
+	var yUp = evt.touches[0].clientY
+
+	var xDiff = xDown - xUp
+	var yDiff = yDown - yUp
+
+	let project
+
+	if (Math.abs(xDiff) > Math.abs(yDiff)) {
+		/*most significant*/
+		if (xDiff > 0) {
+			/* left swipe */
+		} else {
+			/* right swipe */
+		}
+	} else {
+		if (yDiff > 0) {
+			project = document.getElementById(
+				projects[currentProject.index - 1].anchor
+			)
+			project.scrollIntoView({
+				behavior: 'smooth'
+			})
+			currentProject = projects[currentProject.index - 1]
+			onItemClick(currentProject.index)
+		} else {
+			project = document.getElementById(
+				projects[currentProject.index + 1].anchor
+			)
+			project.scrollIntoView({
+				behavior: 'smooth'
+			})
+			currentProject = projects[currentProject.index + 1]
+			onItemClick(currentProject.index)
+		}
+	}
+	/* reset values */
+	xDown = null
+	yDown = null
 }
